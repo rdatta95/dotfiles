e77cc6078de7639f6e5efb43b67c04c29e1dec68
Ritwik Datta <rdatta95@gmail.com>
2021-08-30 14:55:05 +0530

    Extracted prompt to .bash_prompt, added it to symlink and updated the prompt

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-ln -sfv `pwd`/dotfiles/.vimrc ~/.vimrc
-ln -sfv `pwd`/dotfiles/.tmux.conf ~/.tmux.conf
+# bash
 ln -sfv `pwd`/dotfiles/.bashrc ~/.bashrc
+ln -sfv `pwd`/dotfiles/.bash_prompt ~/.bash_prompt
+
+# vim
+ln -sfv `pwd`/dotfiles/.vimrc ~/.vimrc
+
+# tmux
+ln -sfv `pwd`/dotfiles/.tmux.conf ~/.tmux.conf
