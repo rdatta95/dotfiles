06b0e0dddf4ca3ed7ebc51233c71420a38286809
Ritwik Datta <rdatta95@gmail.com>
2021-09-04 18:09:24 +0530

    Removed -f option from ln

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # bash
-ln -fv `pwd`/dotfiles/.bashrc ~/.bashrc
-ln -fv `pwd`/dotfiles/.bash_prompt ~/.bash_prompt
+ln -v `pwd`/dotfiles/.bashrc ~/.bashrc
+ln -v `pwd`/dotfiles/.bash_prompt ~/.bash_prompt
 
 # vim
-ln -fv `pwd`/dotfiles/.vimrc ~/.vimrc
+ln -v `pwd`/dotfiles/.vimrc ~/.vimrc
 
 # tmux
-ln -fv `pwd`/dotfiles/.tmux.conf ~/.tmux.conf
+ln -v `pwd`/dotfiles/.tmux.conf ~/.tmux.conf
